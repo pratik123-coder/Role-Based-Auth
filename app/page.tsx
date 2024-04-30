249cd8482522b64acf55cb0de8f9774b05b66827
pratik123-coder <pratikmohanty939@yahoo.com>
2024-05-01 00:49:19 +0530

    Bug Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-800 to-black">
       <div className="space-y-6 text-center">
          <h1 className="text-6xl font-semibold text-white drop-shadow-md">
-          Authenticator
+          SentryKey : Role Based Authentication App
          </h1>
          <p className="text-white text-lg">Role based authentication app</p>
          <div>
