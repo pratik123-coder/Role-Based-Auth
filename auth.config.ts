78d39bf5f68570fdecf4422664f65804249d9afa
pratik123-coder <pratikmohanty939@yahoo.com>
2024-05-01 22:44:07 +0530

    OAuth Added || Email Verification Removed For OAuth Users || Events Added in Auth.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,7 @@
 import Credentials from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
@@ -8,6 +10,15 @@ import bcrypt from "bcryptjs";
 
  
 export default { providers: [
+    Github({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+    }),
+    Google({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+    
+    }),
     Credentials({
         async authorize(credentials) {
             const validatedFields = LoginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -2,9 +2,38 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import {PrismaAdapter} from "@auth/prisma-adapter"
 
+import { getUserById } from "./data/user"
 import { db } from "./lib/db"
+import { UserRole } from "@prisma/client"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks:{
+    // async signIn({user}){
+    //   if (!user || !user.id) return false;
+    //   const existingUser = await getUserById(user.id);
+    //   if(!existingUser || !existingUser.emailVerified) return false;
+    //   return true;
+    // },
+    async session({token,session}){
+      console.log({
+        sessionToken:token
+      })
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({token}){
+      if(!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if(!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session:{strategy: "jwt"},
   ...authConfig
