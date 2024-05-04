0e5ed0464f8c1405b7d638b130c2e0695f5e569e
pratik123-coder <pratikmohanty939@yahoo.com>
2024-05-04 23:29:13 +0530

    TODO Pages Created || TODO : Functionality

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -3,18 +3,16 @@ import { logout } from "@/actions/logout";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
 
-const Settings = () => {
+const Client = () => {
     const user = useCurrentUser();
     const onClick = () => {
         logout();
     }
     return ( 
         <div className="bg-white p-10 rounded-xl">
-            <button type="submit" onClick={onClick}>
-                Logout
-            </button>
+            
         </div>
      );
 }
  
-export default Settings;
+export default Client;
